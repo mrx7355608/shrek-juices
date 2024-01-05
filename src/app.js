c637a21a694bc1ca7ec8f83a889dfb1b0b1ffc78
mrx7355608 <mrx7355608@gmail.com>
2024-01-05 22:07:53 +0500

    setup express server and handlebars templating engine

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from "express"
+import helmet from "helmet"
+import hpp from "hpp"
+import path from "path"
+import __dirname from "./utils/dirnameImport.js";
+import { engine } from "express-handlebars"
+
+const app = express();
+
+app.use(helmet())
+app.use(hpp())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+// Serve css and images
+app.use(express.static(path.join(__dirname, "..", "..", "/public")))
+/*
+    * TODO: setup view engine
+*/
+
+app.set("view engine", "hbs")
+app.set("views", path.join(__dirname, "..", "/views"))
+app.engine(".hbs", engine({
+    extname: ".hbs",
+    defaultLayout: "main.hbs",
+    layoutsDir: __dirname + "/../views/layouts"
+}))
+
+app.get("/", (req, res) => {
+    res.render("home")
+})
+
+export default app;
---
+++ src/server.js
@@ -0,0 +1,5 @@
+import app from "./app.js"
+
+app.listen(5000, "0.0.0.0", () => {
+    console.log("express started on port 5000")
+})
