ccc7568dec971752df52fe9e37dacf0b430704bc
mrx7355608 <mrx7355608@gmail.com>
2024-01-06 21:30:06 +0500

    installed morgan

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express"
+import morgan from "morgan"
 import helmet from "helmet"
 import hpp from "hpp"
 import path from "path"
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(helmet())
 app.use(hpp())
+app.use(morgan("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 // Serve css and images
