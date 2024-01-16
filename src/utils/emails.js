64aa529dde55ea182b46cf67e3c35944dacd3080
mrx7355608 <mrx7355608@gmail.com>
2024-01-16 12:39:03 +0500

    arranged all emails related code in one file

---
+++ src/utils/emails.js
@@ -0,0 +1,39 @@
+import nodemailer from "nodemailer"
+
+const transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+})
+
+// Send verification email
+async function sendVerificationEmail(receiver, verificationLink) {
+    const mailOptions = {
+        from: process.env.EMAIL_SENDER,
+        to: receiver,
+        subject: "Account Verification",
+        text: `Please verify your account\n${verificationLink}`
+    }
+    await transport.sendMail(mailOptions);
+    console.log("mail sent!")
+}
+
+/*
+    * TODO: send password reset email
+*/
+async function sendPasswordResetEmail() {}
+
+/*
+    * TODO: send order placed email
+*/
+async function sendOrderPlacedEmail() {}
+
+
+export { 
+    sendVerificationEmail,
+    sendPasswordResetEmail,
+    sendOrderPlacedEmail
+}
